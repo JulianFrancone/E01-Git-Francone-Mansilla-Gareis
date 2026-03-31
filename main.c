fdda915bfe31859f6c2f158a6eeaf0469d26ac23
Julian Francone <franconejulian1@gmail.com>
2026-03-31 20:51:23 -0300

    Dev-1: ingreso de dos numeros por teclado

--- main.c
+++ main.c
@@ -6,5 +6,15 @@ void iguales();
 
 int main() {
 
+    // Inicio del trabajo Dev-1	
+    int a, b;
+
+    printf("Ingrese el primer numero: ");
+    scanf("%d", &a);
+
+    printf("Ingrese el segundo numero: ");
+    scanf("%d", &b);
+    // Fin del trabajo Dev-1
+
     return 0;
 }
