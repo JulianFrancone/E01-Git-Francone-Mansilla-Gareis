27c75ea2f989b370f5ecea96dcea740734fb8005
Fabri245-12 <fabrimansi3@gmail.com>
2026-04-05 13:59:54 -0300

    Dev-2: Comparacion de numeros y llamado de funciones

--- main.c
+++ main.c
@@ -5,6 +5,14 @@ void segundo();
 void iguales();
 
 int main() {
-
+    if(primnum>segnum){
+        primero();
+    }
+    else if(primnum<segnum){
+        segundo();
+    }
+    else{
+        iguales();
+    }
     return 0;
 }
