bd9583e28602ef123ddd2cbbf058d7e8610c251e
Julian Francone <franconejulian1@gmail.com>
2026-04-07 13:10:08 -0300

    Dev-1: cambio en mensaje primer numero

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int segnum;
 
 int main() {
 
-    printf("Ingrese el primer numero: ");
+    printf("Dev-1: Ingrese el primer numero: ");
     scanf("%d", &primnum);
 
     printf("Ingrese el segundo numero: ");
