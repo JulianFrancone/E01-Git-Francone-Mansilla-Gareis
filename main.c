ceb14024180794b8ac0ebdcbe86afe7d2f39c784
GareisAgustin <gareisagustin11@gmail.com>
2026-04-07 12:25:05 -0300

    reacomodando

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+void primero();
+void segundo();
+void iguales();
 
-
+extern int prinum;
+ extern int segnum;
 void primero(){
+
     printf("El primer numero es el mayor:%d\n",primnum);
     
 }
 void segundo(){
+   
     printf("El segundo numero es el mayor:%d\n", segnum);
    
 }
 void iguales(){
+   
     printf("Ambos numeros son iguales:%d\n",primnum);
     
 }
