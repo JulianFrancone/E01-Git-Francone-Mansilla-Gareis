575e5a27ae7d004997ebd7eb18cea4d891b485f5
GareisAgustin <gareisagustin11@gmail.com>
2026-04-07 09:39:51 -0300

    quite las variables y edite las impresiones

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-int primnum;
-int segnum;
+
 
 void primero(){
-    printf("El primer numero es el mayor:");
-    printf(primnum);
+    printf("El primer numero es el mayor:%d\n",printf(primnum));
+    
 }
 void segundo(){
-    printf("El segundo numero es el mayor:");
-    printf(segnum);
+    printf("El segundo numero es el mayor:%d\n", printf(segnum));
+   
 }
 void iguales(){
-    printf("Ambos numeros son iguales:");
-    printf(primnum);
+    printf("Ambos numeros son iguales:%d\n",printf(primnum));
+    
 }
 
 int main() {
