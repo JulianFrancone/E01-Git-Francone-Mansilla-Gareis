a2a8925420b83eeddfff1b50fed0a6104e19851f
GareisAgustin <gareisagustin11@gmail.com>
2026-04-07 10:10:10 -0300

    quite el printf repetido

--- main.c
+++ main.c
@@ -2,15 +2,15 @@
 
 
 void primero(){
-    printf("El primer numero es el mayor:%d\n",printf(primnum));
+    printf("El primer numero es el mayor:%d\n",primnum);
     
 }
 void segundo(){
-    printf("El segundo numero es el mayor:%d\n", printf(segnum));
+    printf("El segundo numero es el mayor:%d\n", segnum);
    
 }
 void iguales(){
-    printf("Ambos numeros son iguales:%d\n",printf(primnum));
+    printf("Ambos numeros son iguales:%d\n",primnum);
     
 }
 
